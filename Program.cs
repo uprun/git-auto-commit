4bed5731687dd7cc7b075caaea84142dc785d98b
Oleksandr Kryvonos <geraclion@gmail.com>
2024-11-25 23:35:07 +0100

    2024-11-25--23h35m07s

--- Program.cs
+++ Program.cs
@@ -94,7 +94,7 @@ public class Bundle_Watcher
         Console.WriteLine(fullPath);
         Console.WriteLine($"changes detected as of time: {DateTime.Now:HH-mm-ss:fff}");
 
-        var current_branch = git_current_branch();
+        var current_branch = git_current_branch(full_project_directory_path);
 
         var output_git_add = git_add();
         if (string.IsNullOrEmpty(output_git_add) == false )
