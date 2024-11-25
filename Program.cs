6f872cb853bc6c21a283b248eba01e5c6611a250
Oleksandr Kryvonos <geraclion@gmail.com>
2024-11-25 23:28:29 +0100

    2024-11-25--23h28m29s

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ public class Bundle_Watcher
 
     private void OnChanged(object sender, FileSystemEventArgs e)
     {
-        Path.Ge
+        Path.GetPathRoot()
         if (e.FullPath.Contains("/.git/"))
         {
             Console.WriteLine($"Ignoring {e.FullPath}");
