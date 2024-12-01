cf9d368027cb7ea0c9e4cc4ee8e382ec9b37a947
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-01 22:10:40 +0100

    2024-12-01--22h10m40s

--- string_pipe.cs
+++ string_pipe.cs
@@ -2,6 +2,7 @@ public static class string_pipe
 {
     public static bool pipe_check(this string value, Func<string, bool> rule_to_check)
     {
-        return rule_to_check(value);
+        var result = rule_to_check(value);
+        return result;
     }
 }
