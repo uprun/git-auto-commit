1ee28cd88169041c189cb5b03369b6e85654c755
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-01 22:06:19 +0100

    2024-12-01--22h06m19s

--- Program.cs
+++ Program.cs
@@ -145,7 +145,8 @@ public class Bundle_Watcher
             var (run_process_output, run_process_error) = run_process(full_project_directory_path, "git", "push");
             if (String.IsNullOrEmpty( run_process_error ) == false)
             {
-                var (push_set_upstream_origin__output, push_set_upstream_origin__error ) =  run_process(full_project_directory_path, "git", $"push --set-upstream origin {current_branch}");
+                var (push_set_upstream_origin__output, push_set_upstream_origin__error ) = 
+                 run_process(full_project_directory_path, "git", $"push --set-upstream origin {current_branch}");
 
             }
 
