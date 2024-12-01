a5a6fac37407539d0bbcfe3eac33857a5c8f5d89
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-01 15:01:28 +0100

    2024-12-01--15h01m28s

--- Program.cs
+++ Program.cs
@@ -130,9 +130,7 @@ public class Bundle_Watcher
                 // only allow branching from "main" branch
                 DateTime now = DateTime.Now;
                 var branch_name = $"{now:yyyy-MM-dd--HH}h{now:mm}m";
-                var argumnets = ;
                 run_process(full_project_directory_path, "git", $"checkout -b {branch_name}");
-                git_create_new_branch(full_project_directory_path);
             }
             else
             {
